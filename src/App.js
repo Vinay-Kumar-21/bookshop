f6db166510542c52aa64d23c5b718d6e11ef68b2
Vinay Kumar <vinaykumarraju21@gmail.com>
2024-05-17 10:49:30 +0530

    Hero & bestbooks completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero'
+import BestBooks from './components/BestBooks/BestBooks'
 
 function App() {
   return (
    <>
-      <div>
+      <div className='bg-white dark:bg-gray-900 dark:text-white duration-200'>
         <Navbar/>
         <Hero/>
+        <BestBooks/>
       </div>
     </>
   );
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,6 +18,30 @@ function Hero() {
 
     }
 
+    const ImageList=[
+        {
+            id: 1,
+            img: Book1,
+            title: "His Life will forever be Changed",
+            description:
+              "lorem His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          },
+          {
+            id: 2,
+            img: Book2,
+            title: "Who's there",
+            description:
+              "Who's there lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          },
+          {
+            id: 3,
+            img: Book3,
+            title: "Lost Boy",
+            description:
+              "Lost Boy, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          },
+    ];
+
     return (
     <>
         <div className="min-h-[550px] sm:min-h-[650px] 
@@ -42,11 +66,30 @@ function Hero() {
                     <div className='min-h-[450px] flex
                     justify-center items-center relative
                     order-1 sm:order-2'>
+                        {/* main image */}
                         <div className='h-[300px] sm:h-[450px]
                         overflow-hidden flex justify-center items-center'>
                             <img src={imageId} alt=''
                             className='w-[300px] h-[300px] sm:[h-450px] sm:w-[450px] sm:scale-125
                             object-contain mx-auto'/>
+                        </div>
+
+                        {/* other images */}
+                        <div className='flex lg:flex-col lg:top-1/2
+                        lg:-translate-y-1/2 lg:py-2 justify-center gap-4 absolute
+                        -bottom-[40px] lg:-right-1 bg-white rounded-full'>
+                            {ImageList.map((data)=>(
+                                <img src={data.img} className='max-w-[100px] h-[100px]
+                                object-contain inline-block hover:scale-110 duration-200'
+                                onClick={()=>{
+                                    setImageId(data.id===1 ?
+                                        Book1 : data.id === 2?
+                                        Book2 : Book3
+                                    );
+                                    setTitle(data.title);
+                                    setDescription(data.description);
+                                }}/>
+                            ))}
                         </div>
                     </div>
                 </div>
